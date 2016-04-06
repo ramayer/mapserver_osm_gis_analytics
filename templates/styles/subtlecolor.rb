c19d13ee122f686dee6eac1916ad7b05bb7fbce2
Ron Mayer <ramayer+git@gmail.com>
2016-04-06 08:43:10 -0700

    add another map style to make it easier to see shaded polygons overlaid on the map

--- templates/styles/subtlecolor.rb
+++ templates/styles/subtlecolor.rb
@@ -1,19 +1,25 @@
 # defaults are subtlecolor
 $map_options = {
-       building_color:           '#D1D0CD',          # building
-       building_offset_color:    '#8E8D8C',          # building offset
+       building_color:           '#aaaaaa',          # building
+       building_offset_color:    '#444444',          # building offset
+       hospital_color:           '#E6C8C3',          # hospital
+       land_outline_color_1:	 '#aaaaaa',	     # state outline
+       land_color_1:		 '#f8f8f8',          # basic polygons
+       residential_color:	 '#f8f8f8', 	     # 'residential'    # make this the same as land because some communities don't distinguish
+       industrial_color:	 '#f0f0f0', 	     # airport, military, railways, brownfield, commercial, cemetary, parking
+
+       oneway_color:		 '#9A9ACA',          # the arrows on one-way roads
+
+       park_color:               '#B5D29C',          # golf course, park, recreation_ground, cemetary (other zoom)
+       park_label_color:         '#3D6035',          # park labels
        forest_color:		 '#CCDEAD',          # forest
        grass_color:		 '#DADEAD',          # grass
-       hospital_color:           '#E6C8C3',          # hospital
-       industrial_color:	 '#E3E2DF', 	     # airport, military, railways, brownfield, commercial, cemetary, parking
-       land_color_1:		 '#ffffff',          # basic polygons
-       land_outline_color_1:	 '#aaaaaa',	     # state outline
-       oneway_color:		 '#9A9ACA',          # the arrows on one-way roads
-       park_color:               '#B5D29C',          # golf course, park, recreation_ground, cemetary (other zoom)
+
        rail_color:               '#ffffff',          # rail
        rail_outline_color:       '#A5A5A4',          # rail
        rail_symbol_color:        '#A5A5A4',          # rail
-       residential_color:	 '#EDEAE2', 	     # 'residential'
+
+
        road_color_1:		 '#aa9977',          # major highways
        road_color_2:		 '#ddccaa',          # oregon expressway, etc
        road_color_3:		 '#eeddbb',          # el camino, alma, hwy 9, etc.
@@ -23,6 +29,8 @@ $map_options = {
        road_label_outline_color: '#ffffff',
        road_outline_color_1:	 '#444444',          # road outlines
        road_outline_color_3:	 '#eeddbb',          # road outlines
-       school_color:		 '#DED1AB',          # school, college, university
        water_color:		 '#99b3cc',          # water
+
+       school_color:		 '#ffffaa',          # school, college, university
+
    }
