ba6d5f7a89471452bd8f1072085c5dffbaac0390
Ron Mayer <ramayer+git@gmail.com>
2016-04-24 14:10:02 -0700

    potentially nicer grass/forest/meadow polygon colors

--- templates/styles/higher_contrast.rb
+++ templates/styles/higher_contrast.rb
@@ -2,15 +2,16 @@
 
 $map_colors = {
 
-  industrial:  {color:'#f7f7f7'},
-  land:        {color:'#f7f7f7', outline:'#aaaaaa'},
-  residential: {color:'#f7f7f7'},
+  industrial:  {color:'#f0f0f0'},
+  land:        {color:'#f0f0f0', outline:'#aaaaaa'},
+  residential: {color:'#f0f0f0'},
+  parking:     {color:'#cccccc'},
 
-  park:        {color:'#f7ffef'},
-  grass:       {color:'#eff7e7'},
-  forest:      {color:'#e7efdf'},
+  park:        {color:'#d5e2bc'},
+  grass:       {color:'#e5f2cc'},
+  forest:      {color:'#c5d2ac'},
 
-  building:    {color:'#888888', offset:'#444444'},
+  building:    {color:'#d1d0cd', offset:'#8e8d8c'},
   citycircle:  {color:'#ddcdcd', outline:'#000000', inner:'#000000'},
   footway:     {color:'#7f7f7f'},
 
@@ -18,11 +19,13 @@ $map_colors = {
   oneway:      {color:'#ffffff'},
   pedestrian:  {color:'#aaaaaa'},
   rail:        {color:'#ffffff', outline:'#A5A5A4',symbol:'#A5A5A4'},
+
   road_1:      {color:'#aa9977', outline:'#666666'},
   road_2:      {color:'#bbaa88', outline:'#666666'},
   road_3:      {color:'#ccbb99', outline:'#666666'},
   road_4:      {color:'#ddccaa', outline:'#666666'},
   road_5:      {color:'#aaaaaa'},
+
   school:      {color:'#ffffcc'},
   track:       {outline:'#cccccc',symbol:'#ffffff'},
   tunnel:      {outline:'#444444'},
